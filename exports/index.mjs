2562d5e91ee9f96d42bb8f1e2ce75154878bf2e6
hardfist <yangjianzju@gmail.com>
2025-05-12 10:16:39 +0800

    fix: support named export for cjs

--- exports/index.mjs
+++ exports/index.mjs
@@ -1,5 +1,5 @@
 // ES modules wrapper
 import { ReactRefreshRspackPlugin } from '../dist/index.js';
-
+// default export will be deprecated in next major version
 export default ReactRefreshRspackPlugin;
 export { ReactRefreshRspackPlugin };
