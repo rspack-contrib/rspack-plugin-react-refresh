8c44c212eaf217cea1a76c2599ca73551c3b710f
neverland <chenjiahan.jait@bytedance.com>
2025-10-21 13:08:05 +0800

    perf: use require to reduce cjs-module-lexer overhead (#57)

--- exports/index.mjs
+++ exports/index.mjs
@@ -1,5 +1,9 @@
 // ES modules wrapper
-import { ReactRefreshRspackPlugin } from '../dist/index.js';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+const { ReactRefreshRspackPlugin } = require('../dist/index.js');
+
 // default export will be deprecated in next major version
 export default ReactRefreshRspackPlugin;
 export { ReactRefreshRspackPlugin };
